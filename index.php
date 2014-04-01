ab02527f243370b337c84395a308bab00f61e315
Glenn <d124026@edu.rocwb.nl>
2014-04-01 14:08:47 +0200

    Map constructie
    Als je op sync drukt kan je de lokalemap clonen

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en">
+<head>
+<title>Project_Voetbal</title>
+<!-- meta-tags -->
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<!-- css-stylesheets -->
+<link rel="stylesheet" media="screen" href="css/style.css">
+<!-- jQuery-script -->
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+</head>
+<body>
+
+
+
+</body>
+</html>
