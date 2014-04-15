4cffa5991dabf2571199a5d5f4e5195bfb2acc81
Glenn <d124026@edu.rocwb.nl>
2014-04-15 13:22:32 +0200

    Titel aangepast
    spatie toegevoegd

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml" lang="en">
 <head>
-<title>Project_Voetbal</title>
+<title>Project Voetbal</title>
 <!-- meta-tags -->
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <!-- css-stylesheets -->
